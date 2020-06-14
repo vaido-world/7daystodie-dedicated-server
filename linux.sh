484b0d3929c7335b43e55828ea720d334f4b460d
Feldwor <BoQsc@users.noreply.github.com>
2020-06-14 10:03:10 +0300

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -2,3 +2,10 @@ https://superuser.com/questions/1527250/apt-update-error-with-ubuntu-18-10-cosmi
 sed -i 's/us.archive/old-releases/g' input.txt
 apt-get update
 sudo apt install steamcmd
+
+steamcmd
+force_install_dir  ./7dtd
+app_update 294420
+root@Getspace-VM5728-m5t:/# find . -name 7dtd
+cd ./root/.steam/steamcmd/7dtd
+./startserver.sh ./startserver.sh -configfile=serverconfig.xml
