782d942d69b4be9caf2c3e23730bed3830dc2aac
Feldwor <BoQsc@users.noreply.github.com>
2020-06-14 10:06:01 +0300

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,9 +1,19 @@
 https://superuser.com/questions/1527250/apt-update-error-with-ubuntu-18-10-cosmic-version
 sed -i 's/us.archive/old-releases/g' input.txt
 apt-get update
+
+useradd -m steam
+cd /home/steam
+
+sudo add-apt-repository multiverse
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install lib32gcc1 steamcmd 
+
 sudo apt install steamcmd
 
 steamcmd
+login anonymous
 force_install_dir  ./7dtd
 app_update 294420
 root@Getspace-VM5728-m5t:/# find . -name 7dtd
