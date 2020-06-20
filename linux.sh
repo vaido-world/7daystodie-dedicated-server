7a6443d0ac32e5806247505f09b68eff9961990f
Feldwor <BoQsc@users.noreply.github.com>
2020-06-20 20:16:34 +0300

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -153,6 +153,8 @@ xmlstarlet edit --inplace \
 # Server Admin File name by default is here
 #  ./home/steam/.local/share/7DaysToDie/Saves/serveradmin.xml
 
+# Correction: Loading permissions file at '/root/.local/share/7DaysToDie/Saves/serveradmin.xml'
+
 # In this serveradmin.xml file admin can be asigned by adding Steam ID:
 # BoQsc steamID64 76561198072601792
 
