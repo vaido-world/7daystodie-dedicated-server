a05d2e62ddc4fd1f22007373a8104059a317a992
Feldwor <BoQsc@users.noreply.github.com>
2020-06-20 16:02:57 +0300

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,3 +1,6 @@
+Use Btivise SSH client 8.43
+
+
 https://superuser.com/questions/1527250/apt-update-error-with-ubuntu-18-10-cosmic-version
 sed -i 's/us.archive/old-releases/g' input.txt
 apt-get update
