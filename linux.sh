a9685db5505d50b5a8f18cfb28f870f4eff2e251
Feldwor <BoQsc@users.noreply.github.com>
2020-06-22 20:24:14 +0300

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -161,7 +161,29 @@ xmlstarlet edit --inplace \
 
 # Also needs XML Password import from other file and make git ignore that file since it will hold the password for the control panel
 #?
-  
+
+
+
+# Change Default Game Save Folder for the Server (Also Changes)
+SAVEGAME_FOLDER_PROPERTY="$(xmlstarlet sel -t -v \
+'//property[@name="SaveGameFolder"]/@name' -n /home/steam/.steam/steamcmd/7dtd/serverconfig.xml)"
+
+# Finds AdminFileName Property in the serverconfig.xml file and inserts SaveGameFolder that is relative to the Server Folder
+# By default 7 Days To Die Game Save Folder and therefore AdminFile folder is outside the sever launch and configurations folder
+# This script will force to store GameSaves and AdminFile in the 7DaysToDie server folder relatively.
+[ "$SAVEGAME_FOLDER_PROPERTY" = "" ] && ( 
+xmlstarlet ed -i "/ServerSettings/property[@name='AdminFileName']" -t elem -n newelement -v "" \
+		   -i /ServerSettings/newelement -t attr -n name -v "SaveGameFolder" \
+		   -i /ServerSettings/newelement -t attr -n value -v "./saves" \
+           --rename /ServerSettings/newelement \
+           --value 'property' \
+		   /home/steam/.steam/steamcmd/7dtd/serverconfig.xml
+) || (
+xmlstarlet edit \
+  --update "//property[@name='SaveGameFolder']/@value" \
+  --value "./saves" /home/steam/.steam/steamcmd/7dtd/serverconfig.xml
+)
+
 # Server Admin File name by default is here
 #  ./home/steam/.local/share/7DaysToDie/Saves/serveradmin.xml
 
