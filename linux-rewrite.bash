88f38086cbe0a05b10df0bf7350c4149677efbe6
Feldwor <BoQsc@users.noreply.github.com>
2020-06-24 14:30:50 +0300

    Update linux-rewrite.bash

--- linux-rewrite.bash
+++ linux-rewrite.bash
@@ -7,7 +7,7 @@
 
 
 
-# Configure the Upgrade
+# Configure the Upgrade (debconf)
 # https://superuser.com/questions/1332341/console-setup-causes-apt-get-update-to-hang/1332926#1332926
 # https://people.debian.org/~plessy/DebianInstallerDebconfTemplates.html
   echo "console-setup   console-setup/charmap47 select  UTF-8" > encoding.conf
@@ -17,6 +17,7 @@
   debconf-set-selections encoding.conf
   rm encoding.conf
 
+# Configure the Upgrade (merging configurations)
 # https://serverfault.com/questions/527789/how-to-automate-changed-config-files-during-apt-get-upgrade-in-ubuntu-12/839563#839563
   apt-get --yes --force-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" upgrade
 
