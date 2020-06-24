9d63c5ca2aa7bc570274741fe1eafee3d13ec027
Feldwor <BoQsc@users.noreply.github.com>
2020-06-24 14:28:40 +0300

    Update linux-rewrite.bash

--- linux-rewrite.bash
+++ linux-rewrite.bash
@@ -8,7 +8,6 @@
 
 
 # Configure the Upgrade
-
 # https://superuser.com/questions/1332341/console-setup-causes-apt-get-update-to-hang/1332926#1332926
 # https://people.debian.org/~plessy/DebianInstallerDebconfTemplates.html
   echo "console-setup   console-setup/charmap47 select  UTF-8" > encoding.conf
