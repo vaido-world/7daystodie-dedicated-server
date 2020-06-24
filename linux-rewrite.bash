40b22e824e3e849fd813b824a16b5ddff15b4d26
Feldwor <BoQsc@users.noreply.github.com>
2020-06-24 14:42:22 +0300

    Update linux-rewrite.bash

--- linux-rewrite.bash
+++ linux-rewrite.bash
@@ -52,4 +52,94 @@ steamcmd +login anonymous +force_install_dir ./7dtd +app_update 294420 +quit
 sudo apt-get install -y xmlstarlet
 
 # Configure 7 Days to Die Server 
+# Change Default 7DTD Game Server Name
+xmlstarlet edit --inplace \
+  --update "//property[@name='ServerName']/@value" \
+  --value "[EU][PVP] 7 Days To Die Vanilla Public | XP 1000% | Loot 300%" /home/steam/.steam/steamcmd/7dtd/serverconfig.xml
+  
+# Add Website to the Description of the Game Server in the Lobby
+xmlstarlet edit --inplace \
+  --update "//property[@name='ServerWebsiteURL']/@value" \
+  --value "7days.vaido.world" /home/steam/.steam/steamcmd/7dtd/serverconfig.xml
+  
+# Change Default 7DTD Game Server Description
+xmlstarlet edit --inplace \
+  --update "//property[@name='ServerDescription']/@value" \
+  --value "[EU] 7 Days To Die 
+  Vanilla Public PVP | XP 1000 | Loot 300
+The Dedicated server is based in Europe/Lithuania.
+If you can, support this server. 
+Contact the owner: vaidas.boqsc@gmail.com
+
+The Server is as close to the Vanilla as possible, 
+but focused on less tiring gameplay.
+
+No client side mods.
+
+Any suggestions are Welcome." /home/steam/.steam/steamcmd/7dtd/serverconfig.xml
+
+# Change the Default 8 Player slot to 20
+xmlstarlet edit --inplace \
+  --update "//property[@name='ServerMaxPlayerCount']/@value" \
+  --value "20" /home/steam/.steam/steamcmd/7dtd/serverconfig.xml
+# The amount of Experience the Players get after Killing a zombie, Looting and other activities
+xmlstarlet edit --inplace \
+  --update "//property[@name='XPMultiplier']/@value" \
+  --value "1000" /home/steam/.steam/steamcmd/7dtd/serverconfig.xml
+# Drop on Death Backpack only
+xmlstarlet edit --inplace \
+  --update "//property[@name='DropOnDeath']/@value" \
+  --value "3" /home/steam/.steam/steamcmd/7dtd/serverconfig.xml
+# The amount of Loot that Players will find in the World Containers
+xmlstarlet edit --inplace \
+  --update "//property[@name='LootAbundance']/@value" \
+  --value "300" /home/steam/.steam/steamcmd/7dtd/serverconfig.xml
+# Respawn Loot twice as often
+xmlstarlet edit --inplace \
+  --update "//property[@name='LootRespawnDays']/@value" \
+  --value "15" /home/steam/.steam/steamcmd/7dtd/serverconfig.xml
+ 
+# Let the administrator to join the server even if the server is full
+xmlstarlet edit --inplace \
+  --update "//property[@name='ServerAdminSlots']/@value" \
+  --value "1" /home/steam/.steam/steamcmd/7dtd/serverconfig.xml
+  
+# Make AirDrop Twice as often
+xmlstarlet edit --inplace \
+  --update "//property[@name='AirDropFrequency']/@value" \
+  --value "36" /home/steam/.steam/steamcmd/7dtd/serverconfig.xml
+  
+# Make AirDrop Visible on Map
+xmlstarlet edit --inplace \
+  --update "//property[@name='AirDropMarker']/@value" \
+  --value "true" /home/steam/.steam/steamcmd/7dtd/serverconfig.xml
+  
+# Change the Initial Incorrect 7 Days To Die Dedicated Server PlayerKillingMode
+xmlstarlet edit --inplace \
+  --update "//property[@name='PlayerKillingMode']/@value" \
+  --value "2" /home/steam/.steam/steamcmd/7dtd/serverconfig.xml  
+  
+# Enable Random World Generator for the Server
+xmlstarlet edit --inplace \
+  --update "//property[@name='GameWorld']/@value" \
+  --value "RWG" /home/steam/.steam/steamcmd/7dtd/serverconfig.xml
+  
+# Set a Random Seed for the Random World Generator
+xmlstarlet edit --inplace \
+  --update "//property[@name='WorldGenSeed']/@value" \
+  --value "1354778" /home/steam/.steam/steamcmd/7dtd/serverconfig.xml
+  
+# Set the map size for the Random World Generator
+# Has to be between 2048 and 16384
+xmlstarlet edit --inplace \
+  --update "//property[@name='WorldGenSize']/@value" \
+  --value "2048" /home/steam/.steam/steamcmd/7dtd/serverconfig.xml
+# Set the Map Name for the Random World Generator
+xmlstarlet edit --inplace \
+  --update "//property[@name='GameName']/@value" \
+  --value "VaidoWorld" /home/steam/.steam/steamcmd/7dtd/serverconfig.xml
+# Enable Control Panel for Direct Control of the GameServer
+xmlstarlet edit --inplace \
+  --update "//property[@name='ControlPanelEnabled']/@value" \
+  --value "true" /home/steam/.steam/steamcmd/7dtd/serverconfig.xml
 
