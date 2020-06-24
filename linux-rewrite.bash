2860b2d5e481f4cc0f61d4992e836ad35172f672
Feldwor <BoQsc@users.noreply.github.com>
2020-06-24 23:23:35 +0300

    Update linux-rewrite.bash

--- linux-rewrite.bash
+++ linux-rewrite.bash
@@ -203,5 +203,12 @@ curl https://raw.githubusercontent.com/vaido-world/7daystodie-dedicated-server/m
 unzip StartingItemPlus.zip -d /home/steam/.steam/steamcmd/7dtd/Mods/
 rm StartingItemPlus.zip
 
+# Add Starting Items Plus modlet by Nanashi
+curl https://raw.githubusercontent.com/vaido-world/7daystodie-dedicated-server/master/modlets/STSkipTheStarterQuest-1.0.zip --remote-name
+unzip STSkipTheStarterQuest-1.0.zip -d /home/steam/.steam/steamcmd/7dtd/Mods/
+rm STSkipTheStarterQuest-1.0.zip
+
+
+
 # Start the 7 Days To Die Dedicated Server
 screen /home/steam/.steam/steamcmd/7dtd/startserver.sh -configfile=serverconfig.xml
