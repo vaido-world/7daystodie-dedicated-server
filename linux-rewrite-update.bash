366c4994c8ad50ba1568fa8ae4b525a568293b35
Feldwor <BoQsc@users.noreply.github.com>
2020-07-03 15:36:15 +0300

    Update linux-rewrite-update.bash

--- linux-rewrite-update.bash
+++ linux-rewrite-update.bash
@@ -52,7 +52,7 @@ su - steam <<- 'EOF'
     sudo add-apt-repository multiverse
     sudo dpkg --add-architecture i386
     sudo apt update
-    sudo apt install lib32gcc1 steamcmd 
+    sudo apt install lib32gcc1 steamcmd -y
 EOF
 .
 
