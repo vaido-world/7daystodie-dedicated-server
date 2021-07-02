b3c935352591f311688c5ef529446dd8cca4e4e8
BoQsc <BoQsc@users.noreply.github.com>
2021-07-02 15:02:48 +0300

    oldfilesremoval
    reorganize and remove old files

--- 2020-10-13-7dtd-last-desktop/2020-08-21/html/github_payload.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ( $_POST['payload'] ) {
-shell_exec( "cd /home/steam/.steam/steamcmd/7dtd/; git reset --hard; git add *; git stash push --include-untracked; git pull origin master");
-shell_exec("bash /var/www/html/announce_server_about_update.bash");
-}
-
-?>Github Handle Script loaded without errors.
